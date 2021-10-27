4d6370d14651d9fade838c7b1aa368503bc46bee
emimontanari <emimontanari@icloud.com>
2021-10-26 21:44:31 -0300

    Severjs

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const server = express();
 const path = require("path");
 server.use("/public",express.static("public"));
 
-const HOME_PAGE = "views/home.html";
-const PRODUCT_PAGE = "views/productos.html";
-const CONTACT_PAGE = "views/contacto.html";
+const HOME_PAGE = "views/index.html";
+const PRODUCT_DETAIL = "views/productDetail.html";
 const LOGIN_PAGE = "views/login.html";
-const SEARCH_PAGE = "views/buscar.html";
-const US_PAGE = "views/nosotros.html";
-const CART_PAGE = "views/carrito.html";
-const REGISTER_PAGE = "views/registro.html";
+const PRODUCT_CART = "views/productCart.html";
+const REGISTER_PAGE = "views/registrer.html";
 const PORT = process.env.PORT || 5000;
 
 server.get("/mensaje",(req,res)=>{
@@ -18,37 +15,27 @@ server.get("/mensaje",(req,res)=>{
 });
 
 
-
 server.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,HOME_PAGE));
 });
 
 server.get("/cart",(req,res)=>{
-    res.sendFile(path.join(__dirname,CART_PAGE));
+    res.sendFile(path.join(__dirname,PRODUCT_CART));
 });
 server.get("/registro",(req,res)=>{
     res.sendFile(path.join(__dirname,REGISTER_PAGE));
 });
 
-server.get("/nosotros",(req,res)=>{
-    res.sendFile(path.join(__dirname,US_PAGE));
-});
-
-server.get("/contacto",(req,res)=>{
-    res.sendFile(path.join(__dirname,CONTACT_PAGE));
-});
 
 server.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,LOGIN_PAGE));
 });
 
 server.get("/productos",(req,res)=>{
-    res.sendFile(path.join(__dirname,PRODUCT_PAGE));
+    res.sendFile(path.join(__dirname,PRODUCT_DETAIL));
 });
 
-server.get("/buscar",(req,res)=>{
-    res.sendFile(path.join(__dirname,SEARCH_PAGE));
-});
+
 
 
 server.listen(PORT,()=>{
