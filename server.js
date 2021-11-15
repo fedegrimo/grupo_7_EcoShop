ec32551a375e9cd5762739f3cb149682be75305f
fede.grimo <fede.grimo@gmail.com>
2021-11-15 16:57:05 -0300

    [INSTALL/UPDATE] Liberias / Server.js

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
+// ************ Require's ************
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
 const path = require("path");
 const express = require("express");
+const logger = require('morgan');
+const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
+
+// ************ express() - (don't touch) ************
 const server = express();
-const pageCTRL = require('./controllers/pageCTRL');
+
 const PORT = process.env.PORT || 3000;
 
-//middlewares
+
+// ************ Middlewares - (don't touch) ************
+server.use(express.static(path.join(__dirname, 'public')));  // Necesario para los archivos estáticos en el folder /public
+server.use(express.urlencoded({ extended: false }));
+server.use(logger('dev'));
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
+server.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
 
-//Set Template Engine
-server.set("views", path.resolve(__dirname, "views"));
+// ************ Template Engine - (don't touch) ************
 server.set("view engine", "ejs");
+server.set("views", path.resolve(__dirname, "views"));
 
-//Static Files
+// ************ FOLDERS LIBRARY ************
 server.use('/css',express.static(path.resolve(__dirname,'public/css')));
 server.use('/img',express.static(path.resolve(__dirname,'public/img')));
 server.use('/js',express.static(path.resolve(__dirname,'public/js')));
+
+// ************ WRITE YOUR CODE FROM HERE ************
+// ************ Route System require and use() ************
+const pageCTRL = require('./controllers/pageCTRL');
 
 //Routes Files
 server.get("/",pageCTRL.index);
@@ -26,6 +42,10 @@ server.get("/login",pageCTRL.login);
 server.get("/product",pageCTRL.product);
 server.get("/agregarProducto",pageCTRL.agregarProducto);
 server.get("/editorProductos",pageCTRL.editorProductos);
+
+// ************ DON'T TOUCH FROM HERE ************
+// ************ catch 404 and forward to error handler ************
+server.use((req, res, next) => next(createError(404)));
 
 server.listen(PORT,()=>{
     console.log(`Servidor iniciado en puerto ${PORT}`);
